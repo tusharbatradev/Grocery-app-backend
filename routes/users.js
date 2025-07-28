8467f2b3b18ee2abfae737130042b71669db5009
Tushar Batra <tusharbatra08@gmail.com>
2025-07-28 17:51:11 +0530

    Pagination and Feed Api.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const connectionRequest = require("../Models/connectionRequest");
+const User = require("../Models/users");
+
+const USER_SAFE_DATA = "firstName lastName photoUrl about skills gender age";
 
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   try {
@@ -12,10 +15,7 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
         toUserId: loggedInUser._id,
         status: "interested",
       })
-      .populate(
-        "fromUserId",
-        "firstName lastName photoUrl about skills gender age"
-      );
+      .populate("fromUserId", USER_SAFE_DATA);
 
     res.status(200).json({
       message: "All requests",
@@ -37,28 +37,61 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
           { toUserId: loggedInUser._id, status: "accepted" },
         ],
       })
-      .populate(
-        "fromUserId",
-        "firstName lastName photoUrl about skills gender age"
-      )
-      .populate(
-        "toUserId",
-        "firstName lastName photoUrl about skills gender age"
-      );
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
     const data = connections.map((row) => {
-        if(row.fromUserId._id.toString() === loggedInUser._id.toString()) {
-            return row.toUserId;
-        }
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
 
-        return row.fromUserId
+      return row.fromUserId;
     });
 
     res.status(200).json({
-      data
+      data,
     });
   } catch (err) {
     res.status(400).send("Error : " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    let skip = (page - 1) * limit;
+
+    const connectionRequests = await connectionRequest
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    hideUsersFromFeed.add(loggedInUser._id.toString());
+
+    const feedUsers = await User.find({
+      _id: { $nin: Array.from(hideUsersFromFeed) },
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "Feed users",
+      data: feedUsers,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
